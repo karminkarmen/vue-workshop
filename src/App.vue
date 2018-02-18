1d632f21d0f756dcdb64a4500fe82a1b02b90ffe
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-02-18 09:22:25 +0100

    w4

--- src/App.vue
+++ src/App.vue
@@ -30,13 +30,18 @@
             </p>
           </div>
           <div class="product-list--product--footer">
-            <p class="product-list--product--price price">
-              ${{ product.price }}  <span class="lozenge">free shipping</span>
-            </p>
+            <template v-if="product.inStock > 0">
+              <p class="product-list--product--price price">
+                ${{ product.price }}  <span class="lozenge" v-show="product.price > 20">free shipping</span>
+              </p>
 
-            <div class="product-list--product--actions">
-              <a class="btn" href="#">View product</a>
-            </div>
+              <div class="product-list--product--actions">
+                <a class="btn" href="#">View product</a>
+              </div>
+            </template>
+            <template v-else>
+              <span class="lozenge">out of stock</span> 📦📦📦
+            </template>
           </div>
         </li>
       </ul>
@@ -70,11 +75,13 @@
             </dd>
 
             <dt>Availability:</dt>
-            <dd>a few</dd>
+            <dd v-if="product.inStock <= 0">out of stock</dd>
+            <dd v-else-if="product.inStock < 5">a few</dd>
+            <dd v-else>plenty in stock</dd>
 
             <dt>Price:</dt>
             <dd class="price">
-              ${{ product.price }} <span class="lozenge">free shipping</span>
+              ${{ product.price }} <span v-show="product.price > 20" class="lozenge">free shipping</span>
             </dd>
           </dl>
           <div class="product--footer">
