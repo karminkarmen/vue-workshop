63d93db61e72e30e5048ba5cb2d6f02abb9ace80
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-02-20 15:52:31 +0100

    21.2 mutacje osbono

---
+++ src/mutationKeys.js
@@ -0,0 +1,1 @@
+export const UPDATE_PRODUCT_STATUS = "updateProductsStatus";
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
+import {UPDATE_PRODUCT_STATUS} from '/src/mutationKeys';
 import {getAllProducts} from '/src/productService';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+
 
 export default new Vuex.Store({
   state: {
@@ -17,20 +19,20 @@ export default new Vuex.Store({
   actions: {
     updateProducts({ commit }, newProducts) {
       commit("updateProducts", newProducts);
-      commit("updateProductsStatus", { isLoading: false });
+      commit(UPDATE_PRODUCT_STATUS, { isLoading: false });
     },
     fetchProducts({ getters, commit, dispatch }, pageNumber) {
-      commit("updateProductsStatus", { isLoading: true });
+      commit(UPDATE_PRODUCT_STATUS, { isLoading: true });
       getAllProducts(pageNumber || getters.currentPageNumber)
         .then((products) => dispatch("updateProducts", products))
         .catch(() => {
           dispatch("updateProducts", []);
-          commit("updateProductsStatus", { isError: true })
+          commit(UPDATE_PRODUCT_STATUS, { isError: true })
         });
     }
   },
   mutations: {
-    updateProductsStatus(state, newStatus) {
+    [UPDATE_PRODUCT_STATUS](state, newStatus) {
       state.productsStatus = newStatus;
     },
     updateProducts(state, newProducts) {
