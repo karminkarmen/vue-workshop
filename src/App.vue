ec7fef33610d1f93e6ef5c2f87b148e357fc4822
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-03-01 17:14:09 +0100

    14. Product form with validation

--- src/App.vue
+++ src/App.vue
@@ -11,88 +11,7 @@
 
     <product-details :product="product" />
 
-    <div class="container">
-
-      <form action="#" class="box product-edit">
-        <h2>Edit product X</h2>
-
-        <div class="form-row">
-          <label for="edit-name">Name</label>
-          <input id="edit-name" v-model.trim="product.name" type="text" />
-        </div>
-
-        <div class="form-row">
-          <label for="edit-photo">Photo</label>
-          <input id="edit-photo" v-model.trim.lazy="product.photo" type="text" />
-          <img class="photo-preview" v-style-when-broken :src="product.photo" />
-        </div>
-
-        <div class="form-row">
-          <label for="edit-desc">Description</label>
-          <textarea id="edit-desc" v-model="product.description"></textarea>
-        </div>
-
-        <div class="form-row">
-          <label for="edit-color">Color</label>
-          <input type="color" v-model.trim="product.color" id="edit-color" />
-        </div>
-
-        <div class="form-row">
-          <label>Materials</label>
-          <div>
-            <label><input type="checkbox" value="Steel" v-model="product.materials"/> Steel</label>
-
-          </div>
-        </div>
-
-        <div class="form-row">
-          <label for="edit-departmnet">Department</label>
-          <select id="edit-departmnet" v-model="product.department">
-            <option>Books</option>
-            <option>Movies</option>
-            <option>Music</option>
-            <option>Games</option>
-            <option>Electronics</option>
-            <option>Computers</option>
-            <option>Home</option>
-            <option>Garden</option>
-            <option>Tools</option>
-            <option>Grocery</option>
-            <option>Health</option>
-            <option>Beauty</option>
-            <option>Toys</option>
-            <option>Kids</option>
-            <option>Baby</option>
-            <option>Clothing</option>
-            <option>Shoes</option>
-            <option>Jewelery</option>
-            <option>Sports</option>
-            <option>Outdoors</option>
-            <option>Automotive</option>
-            <option>Industrial</option>
-          </select>
-        </div>
-
-        <div class="form-row">
-          <label for="edit-stock">In stock</label>
-          <input type="number" v-model="product.inStock" id="edit-stock" />
-        </div>
-
-        <div class="form-row">
-          <label for="edit-price">Price</label>
-          <input type="number" v-model.number="product.price" id="edit-price" />
-        </div>
-
-        <button class="btn">Save product</button>
-      </form>
-    </div>
-
-    <div class="container">
-      <div class="box">
-        <div class="spinner"></div>
-      </div>
-    </div>
-
+    <product-form :product="product" />
   </div>
 </template>
 
@@ -103,6 +22,7 @@
   import Navbar from '/src/components/Navbar.vue';
   import ProductDetails from '/src/components/ProdictDetails.vue';
   import ProductsList from '/src/components/ProductsList.vue';
+  import ProductForm from '/src/components/ProductForm.vue';
 
 	export default {
 		data() {
@@ -152,7 +72,8 @@
     components: {
       Navbar,
       ProductDetails,
-      ProductsList
+      ProductsList,
+      ProductForm
     }
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import App from '/src/App.vue'
-import Vue from 'vue'
+import App from '/src/App.vue';
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
 	render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
